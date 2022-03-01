013f9d9a74dd1c3c7faa1b3905387c01b28b2750
dzxion <dzxion@qq.com>
2022-03-01 10:45:10 +0800

    add Quaternion.m

--- Rotation_Matrix.m
+++ Rotation_Matrix.m
@@ -2,11 +2,7 @@ close all;
 clear;
 clc;
 
-syms psi theta phi;
-% syms mbx mby mbz real;
-
-% mbody = [mbx,mby,mbz]';%'
-
+syms psi theta phi real;
 Rz = [cos(psi),-sin(psi),0;
 	  sin(psi), cos(psi),0;
 	         0,        0,1;];
@@ -19,8 +15,29 @@ Rx = [1,       0,        0;
 	  0,cos(phi),-sin(phi);
 	  0,sin(phi), cos(phi);];
 
+%% 磁力计相关
+% syms mbx mby mbz real;
+% mbody = [mbx,mby,mbz]';
 % R_ENU2Body = Rz*Ry*Rx;
 % R_Bodyheading2Body = Ry*Rx;
 % mbodyheading = R_Bodyheading2Body*mbody
 
-R_motor2camare = Rx * Ry
+%% 云台相关
+syms x_dot y_dot z_dot real;% 电机角速度
+wx_m = [x_dot 0 0]';
+wy_m = [0 y_dot 0]';
+wz_m = [0 0 z_dot]';
+
+R_m2c_x = Ry;
+R_c2m_x = R_m2c_x';
+
+R_m2c_z = Rx * Ry;
+R_c2m_z = R_m2c_z';
+
+w_c = wy_m + R_c2m_x*wx_m + R_c2m_z*wz_m;
+
+X = [cos(theta),0,-cos(phi)*sin(theta);
+              0,1,            sin(phi);
+     sin(theta),0, cos(phi)*cos(theta)];
+Y = inv(X);
+simplify(Y)
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 Rotation_Matrix.m 推导旋转矩阵相关的公式
+
+Quaternion.m 推导四元数相关的公式
